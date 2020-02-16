0115b5947f8808f98cffd010480c670bf61c157f
Wang Yaoshen <wangyaoshen@bgi.com>
2020-02-16 21:08:21 +0800

    add title for err.fra.count

--- script/cal.err.fra.count.sh
+++ script/cal.err.fra.count.sh
@@ -1,1 +1,3 @@
-for i in {01..59};do perl script/err_fra_count_20200208.pl workdir/input/F$i.vcf.gz workdir/output/F$i;done > tee workdir/output/err.fra.count.txt
+#!/bin/bash
+echo -e "prefix\tsnpa\tsnpb\terr_rate\tfetal_fra1\tfetal_fra2" |tee workdir/output/err.fra.count.txt
+for i in {01..59};do perl script/err_fra_count_20200208.pl workdir/input/F$i.vcf.gz workdir/output/F$i;done |tee -a workdir/output/err.fra.count.txt
